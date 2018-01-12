9fb2fc5936987760f1462199e9cb9ec3016df33d
Bailee Sjostrom <20sjobai@student.mcpsmt.org>
2018-01-12 10:03:23 -0700

    Initial commit

---
+++ Appointments.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Appointments here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Appointments
+{
+   public static void main() {
+       Scanner in = new Scanner(System.in);
+       
+       System.out.print("Enter Something");
+       String s = in.nextLine();
+       
+       System.out.println(s);
+    }
+}
